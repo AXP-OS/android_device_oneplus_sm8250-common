1eeaa8990383e26b7eed7d1ce78964e0ac796246
steadfasterX <steadfasterX@users.noreply.github.com>
2025-02-24 16:47:12 +0100

    mk: enable AXP.OS

--- common.mk
+++ common.mk
@@ -477,3 +477,6 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
+
+# enable AXP.OS vendor
+$(call inherit-product, vendor/axp/config/common.mk)
